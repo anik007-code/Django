c45ba518306c69104c8ae24a18ada074420166f0
anik <aniklpu01@gmail.com>
2024-10-13 21:27:48 +0600

    added tailwind css

--- portfolio/my_portfolio/urls.py
+++ portfolio/my_portfolio/urls.py
@@ -5,7 +5,6 @@ urlpatterns = [
     path('', views.dashboard, name="dashboard"),
     path("home/", views.home, name="home"),
     path("about/", views.about, name="about"),
-    path("contact", views.contact, name="contact"),
+    path("contact/", views.contact, name="contact"),
     path("blog/", views.blog, name="blog"),
-
     ]
