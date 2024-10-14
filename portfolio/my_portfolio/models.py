36ddf81f14d6b636192a3f48212f2b169e18593a
anik <aniklpu01@gmail.com>
2024-10-14 23:10:10 +0600

    added tailwind css

--- portfolio/my_portfolio/models.py
+++ portfolio/my_portfolio/models.py
@@ -6,3 +6,6 @@ class Contact(models.Model):
     name = models.CharField(max_length=40)
     email = models.EmailField()
     desc = models.TextField()
+
+    def __str__(self):
+        return self.name
